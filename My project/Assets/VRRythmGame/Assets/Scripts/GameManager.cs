b1de86ecf8f6c4f178403780f9bc4a6e248a48e6
kenkaneki3000 <austin.h823@stu.nebo.edu>
2022-10-06 12:28:57 -0600

    beatsaber

---
+++ My project/Assets/VRRythmGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public float startTime = 3.0f;
+    public int score = 0;
+    public float lifeTime = 1.0f;
+    public int hitBlockScore = 10;
+    public float missBlockLife = 0.1f;
+    public float wrongBlockLife = 0.08f;
+    public float lifeRegenRate = 0.1f;
+    public float swordHitVelocityThreshold = 0.5f;
+
+    public VelocityTracker leftSwordTracker;
+    public VelocityTracker rightSwordTracker;
+
+    // instance
+    public static GameManager instance;
+
+    void Awake ()
+    {
+        instance = this;
+    }
+
+    // called when we hit the correct block
+    public void AddScore ()
+    {
+        score += hitBlockScore;
+        GameUI.instance.UpdateScoreText();
+    }
+
+    // called when the block goes past the player
+    public void MissBlock ()
+    {
+        lifeTime -= missBlockLife;
+    }
+
+    // called when we hit the wrong block
+    public void HitWrongBlock ()
+    {
+        lifeTime -= wrongBlockLife;
+    }
+
+    void Update ()
+    {
+        // increase lifetime over time
+        lifeTime = Mathf.MoveTowards(lifeTime, 1.0f, lifeRegenRate * Time.deltaTime);
+
+        if(lifeTime <= 0.0f)
+            LoseGame();
+
+        // update the lifetime bar
+        GameUI.instance.UpdateLifetimeBar();
+    }
+
+    // called when the song is over
+    public void WinGame ()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // called when the lifetime hits 0
+    public void LoseGame ()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ My project/Assets/VRRythmGame/Assets/Scripts/GameUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameUI : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public Image lifetimeBar;
+
+    public static GameUI instance;
+
+    void Awake ()
+    {
+        instance = this;
+    }
+
+    // updates the score text
+    public void UpdateScoreText ()
+    {
+        scoreText.text = string.Format("SCORE\n{0}", GameManager.instance.score.ToString());
+    }
+
+    // updates the lifetime bar
+    public void UpdateLifetimeBar ()
+    {
+        lifetimeBar.fillAmount = GameManager.instance.lifeTime;
+    }
+}
